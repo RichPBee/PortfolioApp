b43bafa1635c61f77494cb97b9e771299899d7b8
ArrBee <81438594+RichPBee@users.noreply.github.com>
2023-12-12 03:06:27 +0000

    SkillCards Abstracted
    Skills section now uses a custom ListCard component to generate the cards programatically. Makes the code for skills much cleaner, and the ListCard can be used elsewhere.

---
+++ src/components/ListCard.tsx
@@ -0,0 +1,24 @@
+interface IListCardProps {
+    classProp: string;
+    title: string;
+    list: string[];   
+}
+
+
+function ListCard({classProp, title, list} : IListCardProps): React.JSX.Element{
+
+    const listItems = list.map((item) => <li>{item}</li>)
+
+    return (
+        <div className={classProp}>
+            <h4>
+                {title}
+            </h4>
+            <ul>
+                {listItems}
+            </ul>
+        </div>
+    );
+};
+
+export default ListCard;
