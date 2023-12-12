925cf339d4fdcd5f645fd920f960790d3dc125db
ArrBee <81438594+RichPBee@users.noreply.github.com>
2023-12-12 23:23:55 +0000

    First Style Pass
    First proper styling pass complete. General styling on desktop is complete, but will likely need some more refining to look more sleek.
    Responsiveness will also be covered in another pass.
    The CSS selectors should also be tidied up within each component to avoid having so many child/relational selectors in the CSS itself.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,15 +6,15 @@ function Footer(): React.JSX.Element
 {
     return (
         <footer className="PageFooter">
-            <div className="LinkButtons">
+            <div className="FooterLinks">
                     <a href="" target="_blank">
-                        <IoDocumentText size={30}/>
+                        <IoDocumentText size={40}/>
                     </a>
                     <a href="https://github.com/RichPBee" target="_blank">
-                        <FaGithub size={30} />
+                        <FaGithub size={40} />
                     </a>
                     <a href="https://www.linkedin.com/in/rich-balfourth-16a54324b/" target="_blank">
-                        <FaLinkedin size={30} />
+                        <FaLinkedin size={40} />
                     </a>
                 </div>
         </footer>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,6 @@
 interface IProjectCardProps {
     classProp: string;
+    id: string;
     title: string;
     buttonInfo: 
     {
@@ -9,17 +10,17 @@ interface IProjectCardProps {
 
 }
 
-function ProjectCard({classProp, title, buttonInfo}: IProjectCardProps) 
+function ProjectCard({classProp, id, title, buttonInfo}: IProjectCardProps) 
 {
 
     const buttonClick = (url: string) => { 
         window.open(url, '_blank');
     }
-    const createButtons = buttonInfo.map((info, index) => <button key={info.text+'-'+index}onClick={() => buttonClick(info.url)}>{info.text}</button>)
+    const createButtons = buttonInfo.map((info, index) => <button className={`button-${index}`} key={info.text+'-'+index}onClick={() => buttonClick(info.url)}>{info.text}</button>)
 
     return (
         <div className={classProp}>
-            <div className="ProjectCardBackground">
+            <div className="ProjectCardBackground" id={id}>
                 <h1>{title}</h1>
                 <div className="CardButtons">
                     {createButtons}
